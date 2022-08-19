85732f6607a3698e760a82a94bba79123bbee8bf
jeanbu <jean.bustinza@gmail.com>
2022-08-19 13:41:53 +0100

    Project setup

---
+++ reminder.rb
@@ -0,0 +1,42 @@
+=begin
+As a user
+So that I can keep track of my tasks
+I want a program that I can add todo tasks to and see a list of them.
+
+As a user
+So that I can focus on tasks to complete
+I want to mark tasks as complete and have them disappear from the list.
+=end
+
+class Reminder
+    def initialize(name) # name is a string
+      @name = name
+      @list = []
+    end
+  
+    def show_tasks(task) # task is a string
+      @todo = task
+      @list << @todo
+    end
+  
+
+
+    def remind()
+      # Throws an exception if no task is set
+      fail “List is empty!” if @list.empty?
+      # Otherwise, returns a string reminding the user to do the task
+      puts @list
+    end
+
+    def mark_completed(task)
+      
+    end
+  end
+
+# EXAMPLE
+
+reminder = Reminder.new("Kay")
+reminder.show_tasks("Walk the dog")
+reminder.show_tasks("Go to the gym")
+reminder.remind()
+
